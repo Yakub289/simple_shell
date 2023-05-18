a8c324add3597b149453965dad26adb5d0535c42
Yakub289 <Yakubsuleiman24@gmail.com>
2023-05-18 06:50:54 -0700

    Memory addressing

---
+++ aux_memo.c
@@ -0,0 +1,24 @@
+#include "shell.h"
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * aux_free - Function that will free a pointer and ineffect address.
+ * @pt: address to access pointer to free.
+ *
+ * Return: 1 if free or Nothing 0.
+ */
+int aux_free(void **pt)
+{
+	if (pt && *pt)
+	{
+		free(*pt);
+		*pt = NULL;
+		return (1);
+	}
+
+	return (0);
+}
+
