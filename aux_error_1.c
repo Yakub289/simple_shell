ca8a9d8f3b763e52410c78e8f854c9c690505ec5
Yakub289 <Yakubsuleiman24@gmail.com>
2023-05-24 13:53:00 -0700

    Error indicators

--- aux_error_1.c
+++ aux_error_1.c
@@ -51,3 +51,94 @@ void get_error(var_s *vars, char *e_str)
 	_post(": ");
 	_post(e_str);
 }
+
+/**
+ * get_di - Write a function that produce a digit to (base 10).
+ * @input: the input
+ * @fl: print line
+ *
+ * Return: number of characters printed
+ */
+int get_di(int input, int fl)
+{
+	int (*__putchar)(char) = _putchar;
+	int p, count = 0;
+	unsigned int _abs_, current;
+
+	if (fl == STDERR_FILENO)
+		__putchar = _postchar;
+	if (input < 0)
+	{
+		_abs_ = -input;
+		__putchar('-');
+		count++;
+	}
+	else
+		_abs_ = input;
+	current = _abs_;
+	for (p = 1000000000; p > 1; p /= 10)
+	{
+		if (_abs_ / p)
+		{
+			__putchar('0' + current / p);
+			count++;
+		}
+		current %= p;
+	}
+	__putchar('0' + current);
+	count++;
+	return (count);
+}
+
+/**
+ * trans_number - Allocate determinant to switch a function case.
+ * @num: number.
+ * @base: base.
+ * @flags: argument flags
+ *
+ * Return: string
+ */
+char *trans_number(long int num, int base, int flags)
+{
+	static char *array;
+	static char buffer[50];
+	char sign = 0;
+	char *pt;
+	unsigned long digit = num;
+
+	if (!(flags & CONVERT_UNSIGNED) && num < 0)
+	{
+		digit = -num;
+		sign = '-';
+	}
+	array = flags & CONVERT_LOWERCASE ? "0123456789abcdef" : "0123456789ABCDEF";
+	pt = &buffer[49];
+	*pt = '\0';
+
+	do	{
+		*--pt = array[digit % base];
+		digit /= base;
+	} while (digit != 0);
+
+	if (sign)
+		*--pt = sign;
+	return (pt);
+}
+
+/**
+ * delete_comment - Function that replaces first initialize of '#' with '\0'.
+ * @buf: String modifying buff.
+ *
+ * Return: Always 0 (success).
+ */
+void delete_comment(char *buf)
+{
+	int d;
+
+	for (d = 0; buf[d] != '\0'; d++)
+		if (buf[d] == '#' && (!d || buf[d - 1] == ' '))
+		{
+			buf[d] = '\0';
+			break;
+		}
+}
